5e213566e830cc007ffa7e980e84f7bf3faded11
Matias <matiasklingeman114@outlook.com>
2024-04-11 10:53:13 -0300

    initial

--- Ejercicio 1 - Guia 5/src/Main.java
+++ Ejercicio 1 - Guia 5/src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner teclado = new Scanner(System.in);
+        String nombre;
+        char sexo;
+        int edad;
+        double peso;
+        double altura;
+
+        System.out.println("Ingrese nombre: ");
+        nombre = teclado.nextLine();
+
+        System.out.println("Ingese edad: ");
+        edad = teclado.nextInt();
+
+        System.out.println("Ingrese el sexo: ");
+        sexo = teclado.nextLine().charAt(0);
+
+
+        System.out.println("Ingrese peso: ");
+        peso = teclado.nextDouble();
+
+        System.out.println("Ingrese altura: ");
+        altura = teclado.nextDouble();
+
+        Persona persona = new Persona(nombre, edad, sexo, peso, altura);
+        System.out.println(persona);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ Ejercicio 1 - Guia 5/src/Persona.java
@@ -0,0 +1,126 @@
+import java.lang.Math;
+import java.util.Random;
+
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private int dni;
+    private char sexo;
+    private double altura;
+    private double peso;
+
+
+    private static final char SEXO_POR_DEFECTO = 'H';
+    private static final int PESO_IDEAL_BAJO = -1;
+    private static final int PESO_IDEAL_NORMAL = 0;
+    private static final int SOBREPESO = 1;
+
+
+    public Persona(){
+        this.nombre = "";
+        this.edad = 0;
+        this.sexo = SEXO_POR_DEFECTO;
+        this.altura = 0;
+        this.peso = 0;
+    }
+
+    public Persona(String nombre, int edad, char sexo, double altura, double peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = generaDNI();
+        this.sexo = comprobarSexo(sexo) ? sexo : SEXO_POR_DEFECTO;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public Persona(String nombre, int edad, char sexo){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int calcularIMC(){
+        double peso = this.peso / Math.pow(this.altura,2);
+
+        if(peso < 20){
+            return PESO_IDEAL_BAJO;
+        }else if( peso >=20 && peso <= 25){
+            return PESO_IDEAL_NORMAL;
+        }else{
+            return PESO_IDEAL_NORMAL;
+        }
+    }
+
+    public boolean esMayorDeEdad(){
+        return this.edad >= 18;
+
+    }
+
+    private boolean comprobarSexo(char sexo){
+        return sexo == 'H' || sexo == 'M';
+    }
+
+    private int generaDNI(){
+        Random random = new Random();
+
+        return random.nextInt(90000000) + 10000000;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni=" + dni +
+                ", sexo=" + sexo +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+    }
+}
